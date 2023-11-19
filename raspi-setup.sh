2c14f98f4c01f0576bee9af6b551a6852de6ba6d
sk <git@bubblebot.de>
2023-11-19 16:47:17 +0100

    RASPI-Setup: added timeouts

--- raspi-setup.sh
+++ raspi-setup.sh
@@ -7,27 +7,16 @@
 #  ██████╔╝╚██████╔╝╚██████╗██║  ██╗███████╗██║  ██║
 #  ╚═════╝  ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
 
-## From https://docs.docker.com/engine/install/raspberry-pi-os/
-echo "Removing previous versions of Docker..."
-# TODO: Enable!
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg -y; done
-
-# Add Docker's official GPG key:
-echo "Setting up official Docker repository..."
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/raspbian \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
 # Install Docker
 echo "Installing Docker..."
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update && apt upgrade -y
+sudo apt install docker docker-compose -y && sudo service docker start
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+docker-compose down
+export DOCKER_CLIENT_TIMEOUT=120
+export COMPOSE_HTTP_TIMEOUT=120
 
 
 #  ███╗   ██╗ ██████╗ ████████╗███████╗ █████╗ ██╗     ██╗     ██████╗ ██╗   ██╗███╗   ██╗██╗  ██╗████████╗
@@ -41,5 +30,9 @@ echo "Setting permissions..."
 chmod -R 744 temp
 chmod -R 744 www/htdocs/minified
 
-echo "Starting container..."
-docker-compose up --build
+echo "Please logout and login again, then execute these commands:"
+echo "cd /opt/notfallpunkt"
+echo "docker network create notfallpunkt-net"
+echo "docker-compose up --build"
+
+
